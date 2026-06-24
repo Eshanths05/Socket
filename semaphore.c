42908d74b394ac65f9be0b453356c51adba64f69
eshanth <eshanths05@gmail.com>
2026-06-24 19:09:27 +0530

    Create semaphore.c

---
+++ semaphore.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+sem_t sem;
+
+void *task(void *arg)
+{
+    sem_wait(&sem);
+
+    printf("Thread %ld is running\n", (long)arg);
+
+    sem_post(&sem);
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t t1, t2;
+
+    sem_init(&sem, 0, 1);
+
+    pthread_create(&t1, NULL, task, (void *)1);
+    pthread_create(&t2, NULL, task, (void *)2);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    sem_destroy(&sem);
+
+    return 0;
+}
